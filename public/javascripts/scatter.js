8b644efa1840256b9db9bda514750e871a916d7e
jon ward <jward@college.harvard.edu>
2015-05-21 00:16:04 -0400

    updated landing page

--- public/javascripts/scatter.js
+++ public/javascripts/scatter.js
@@ -1,4 +1,4 @@
-//from weiglemc's block 6185069
+//credit to weiglemc's block 6185069
 
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
@@ -27,35 +27,75 @@ var yValue = function(d) { return d.tripTime; },
 var cValue = function(d) { return d.route; },
     color = d3.scale.category10()
 
+//add the graph canvas to the body of the webpage
 var svg = d3.select("body").append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform","translate(" + margin.left + "," + margin.top + ")");
+
+//add the tooltip area to the webpage
+var tooltip = d3.select("body").append("div")
+                .attr("class", "tooltip")
+                .style("opacity", 0)
+
+//load data
 d3.json("json/sf-la.json", function(error,data){
   if (error) return console.error(error);
-  console.log(data);
+  data.routes.forEach(function(route) {
+    route.cost = +route.cost;
+    route.tripTime = +route.tripTime
+  });
+
+  xScale.domain([d3.min(data.routes,xValue)-1, d3.max(data.routes,xValue)+1]);
+  yScale.domain([d3.min(data.routes,yValue)-1, d3.max(data.routes,yValue)+1]);
+  
+  // x-axis
+  svg.append("g")
+     .attr("class", "x axis")
+     .attr("transform", "translate(0," + height + ")")
+     .call(xAxis)
+     .append("text")
+     .attr("class", "label")
+     .attr("x", width)
+     .attr("y", -6)
+     .style("text-anchor", "end")
+     .text("Cost (in millions of dollars)")
+  
+  // y-axis
+  svg.append("g")
+     .attr("class", "y axis")
+     .call(yAxis)
+     .append("text")
+     .attr("class", "label")
+     .attr("transform", "rotate(-90)")
+     .attr("y", -6)
+     .attr("dy", ".71em")
+     .style("text-anchor", "end")
+     .text("Trip Time (in minutes)")
+ 
+  // draw dots
+  svg.selectAll(".dot")
+     .data(data.routes)
+     .enter()
+     .append("circle")
+     .attr("class", "dot")
+     .attr("r", 7)
+     .attr("cx",xMap)
+     .attr("cy",yMap)
+     .style("fill", function(d) {return color(cValue(d));})
+     .on("mouseover", function(d) {
+        tooltip.transition()
+               .duration(200)
+               .style("opacity", .9);
+        tooltip.html("tooltip")
+               .style("left", (d3.event.pageX + 5) + "px")
+               .style("top", (d3.event.pageY - 28) + "px");
+     })
+     .on("mouseout", function(d) {
+        tooltip.transition(500)
+               .style("opacity", 0);
+     });
 });
 
-svg.append("g")
-   .attr("class", "x axis")
-   .attr("transform", "translate(0," + height + ")")
-   .call(xAxis)
-   .append("text")
-   .attr("class", "label")
-   .attr("x", width)
-   .attr("y", -6)
-   .style("text-anchor", "end")
-   .text("Cost (in millions of dollars)")
-
-svg.append("g")
-   .attr("class", "y axis")
-   .call(yAxis)
-   .append("text")
-   .attr("class", "label")
-   .attr("transform", "rotate(-90)")
-   .attr("y", -6)
-   .attr("dy", ".71em")
-   .style("text-anchor", "end")
-   .text("Trip Time (in minutes)")
 
--- public/javascripts/us.js
+++ public/javascripts/us.js
@@ -29,28 +29,79 @@ d3.json("json/us.json", function(error, us) {
     return lonlats.map( function(lonlat) { return lonlat_to_xy(lonlat,projection)})
   }
 
-  var xyCoords=lonlats_to_xys([[-118.23, 34.05],[-122.4167, 37.7833]],projection);
-  //var cities = {"SF":{"lat": "37.7833","lon": "-122.4167},"LA":{"lat": "34.05","lon": "-118.23"}}
-  // var pairs = {}     
-  var numCities = xyCoords.length;   
+d3.json("json/cityPairs.json", function(error, data) {
+  if (error) return console.error(error);
+  data.cityPairs.forEach(function(pair){
 
-  for(i = 0; i < numCities; i++){
+    console.log(pair.startLatLng.Lng)      
+    console.log(pair.startLatLng.Lat)
+    console.log(pair.endLatLng.Lng)
+    console.log(pair.endLatLng.Lat)
+
+    svg.append("line")
+       .attr("x1",)//pair.startLatLng.Lng)
+       .attr("y1",)//pair.startLatLng.Lat)
+       .attr("x2",)//pair.endLatLng.Lng)
+       .attr("y2",)//pair.endLatLng.Lat)
+       .attr("stroke-width", 4)
+       .attr("stroke", "green")
+       .on("mouseover", mouseOver)
+       .on("mouseout", mouseOut)
+       .on("click", function(d,i){          
+            window.location.href += "citypair";
+        });
+
     svg.append("circle")
-       .attr("cx",xyCoords[i][0])
-       .attr("cy",xyCoords[i][1])
+       .attr("cx",)
+       .attr("cy",)
        .attr("r", "8px")
-       .attr("fill", "red")
-  }
+       .attr("fill", "blue")   
+      
+    svg.append("circle")
+       .attr("cx",pair.endLatLng.Lng)
+       .attr("cy",pair.endLatLng.Lng)
+       .attr("r", "8px")
+       .attr("fill", "blue")
+
+  });
+});
+  /*
+  var xyCoords=lonlats_to_xys([[-118.23, 34.05],[-122.4167, 37.7833]],projection);
+  var numCities = xyCoords.length;   
 
   svg.append("line")
      .attr("x1",xyCoords[0][0])
      .attr("y1",xyCoords[0][1])
      .attr("x2",xyCoords[1][0])
      .attr("y2",xyCoords[1][1])
-     .attr("stroke-width", 2)
-     .attr("stroke", "black")
+     .attr("stroke-width", 4)
+     .attr("stroke", "green")
+     .on("mouseover", mouseOver)
+     .on("mouseout", mouseOut)
      .on("click", function(d,i){          
             window.location.href += "citypair";
         });
 
+  for(i = 0; i < numCities; i++){
+    svg.append("circle")
+       .attr("cx",xyCoords[i][0])
+       .attr("cy",xyCoords[i][1])
+       .attr("r", "8px")
+       .attr("fill", "blue")
+  }
+  */
+  function mouseOver(d) {
+    d3.select(this)
+      .transition()
+      .duration(300)
+      .style('stroke-width', 8);
+  }
+  
+  function mouseOut(d) {
+    d3.select(this)
+      .transition()
+      .duration(300)
+      .style('stroke-width', 4)
+  }
+   
 });
